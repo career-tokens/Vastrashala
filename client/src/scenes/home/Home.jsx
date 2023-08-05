2039712fd2a4ed4c272371adef459d365ac15c7a
mainak <heellomeelo@gmail.com>
2023-08-05 12:13:53 +0530

    first commit

---
+++ client/src/scenes/home/Home.jsx
@@ -0,0 +1,16 @@
+import ShoppingList from "./ShoppingList";
+//import Subscribe from "./Subscribe";
+import MainCarousel from "./MainCarousel";
+
+function Home() {
+  return (
+    <div className="home">
+      <MainCarousel />
+      <ShoppingList />
+      {/**
+      <Subscribe />*/}
+    </div>
+  );
+}
+
+export default Home;
