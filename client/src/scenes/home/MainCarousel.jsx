8d4cd8426dd82dfe052bdc5de7548d433a22abd2
mainak <heellomeelo@gmail.com>
2023-08-08 08:55:04 +0530

    changes

--- client/src/scenes/home/MainCarousel.jsx
+++ client/src/scenes/home/MainCarousel.jsx
@@ -62,34 +62,12 @@ const MainCarousel = () => {
             alt={`carousel-${index}`}
             style={{
               width: "100%",
-              height: "700px",
+              height: "auto",
               objectFit: "cover",
               backgroundAttachment: "fixed",
+              marginTop:"10vh",
             }}
           />
-          <Box
-            color="white"
-            padding="20px"
-            borderRadius="1px"
-            textAlign="left"
-            backgroundColor="rgb(0, 0, 0, 0.4)"
-            position="absolute"
-            top="46%"
-            left={isNonMobile ? "10%" : "0"}
-            right={isNonMobile ? undefined : "0"}
-            margin={isNonMobile ? undefined : "0 auto"}
-            maxWidth={isNonMobile ? undefined : "240px"}
-          >
-            <Typography color={shades.secondary[200]}>-- NEW ITEMS</Typography>
-            <Typography variant="h1">Summer Sale</Typography>
-            <Typography
-              fontWeight="bold"
-              color={shades.secondary[300]}
-              sx={{ textDecoration: "underline" }}
-            >
-              Discover More
-            </Typography>
-          </Box>
         </Box>
       ))}
     </Carousel>
